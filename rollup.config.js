c83be30d6c8c3b6a39b0a979e84f3e62c6f78692
Nick Hingston <nick@wearemothership.com>
2024-05-31 07:43:05 +0100

    fix for node v20

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,28 @@
+// import pluginNodeResolve from 'rollup-plugin-node-resolve';
+//This plugin prevents packages listed in peerDependencies from being bundled with our component library
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import typescript from '@rollup/plugin-typescript';
+
+//efficiently bundles third party dependencies we've installed and use in node_modules
+import resolve from '@rollup/plugin-node-resolve';
+
+// //enables transpilation into CommonJS (CJS) format
+import commonjs from '@rollup/plugin-commonjs';
+
+//transpiled our TypeScript code into JavaScript. This plugin will use all the settings we have set in tsconfig.json.
+//We set "useTsconfigDeclarationDir": true so that it outputs the .d.ts files in the directory specified by in tsconfig.json
+import typescript from 'rollup-plugin-typescript2';
+
+import packageJson from './package.json';
 
 export default {
   input: 'src/index.tsx',
   output: [
     {
-      file: 'lib/index.cjs',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: 'lib/index.esm.js',
+      file: packageJson.module,
       format: 'esm',
       sourcemap: true,
     },
   ],
-  plugins: [peerDepsExternal(), typescript()],
+  plugins: [peerDepsExternal, resolve, commonjs, typescript()],
+  external: ['react', 'react-dom'],
 };
